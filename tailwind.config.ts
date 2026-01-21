903d59dec04a95d465fc91f4575c42c6ba3d21d1
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-01-21 17:15:07 -0600

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["ui-sans-serif", "system-ui", "Segoe UI", "Roboto", "Helvetica", "Arial"],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
